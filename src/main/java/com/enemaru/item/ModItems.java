eed253dfe8e4952484a2af8b0f315dbfa8c41619
ryo <rtasansax@gmail.com>
2025-07-31 00:23:21 +0900

    ➕ add: オリジナルアイテム実装（コミット漏れ）

--- src/main/java/com/enemaru/item/ModItems.java
+++ src/main/java/com/enemaru/item/ModItems.java
@@ -1,4 +1,34 @@
 package com.enemaru.item;
 
+import com.enemaru.Enemaru;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
 public class ModItems {
+    public static void initialize(){
+        // Get the event for modifying entries in the ingredients group.
+        // And register an event handler that adds our suspicious item to the ingredients group.
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS)
+                .register((itemGroup) -> itemGroup.add(ModItems.SUSPICIOUS_SUBSTANCE));
+    }
+
+    public static Item register(Item item, String id){
+        // Create the identifier for the item.
+        Identifier itemID = Identifier.of(Enemaru.MOD_ID, id);
+
+        // Register the item.
+        Item registeredItem = Registry.register(Registries.ITEM, itemID, item);
+
+        // Return the registered item!
+        return registeredItem;
+    }
+
+    public static final Item SUSPICIOUS_SUBSTANCE = register(
+            new Item(new Item.Settings()),
+            "suspicious_substance"
+    );
 }
