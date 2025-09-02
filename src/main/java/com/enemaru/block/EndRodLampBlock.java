b1a43175e5c7ff277d01e263832ac2f841f1a00d
32229Tsuge-EE <32229@toyota.kosen-ac.jp>
2025-09-02 16:46:41 +0900

    エンドロッドの向きを変える処理を追加

--- src/main/java/com/enemaru/block/EndRodLampBlock.java
+++ src/main/java/com/enemaru/block/EndRodLampBlock.java
@@ -10,30 +10,42 @@ import net.minecraft.block.entity.BlockEntityTicker;
 import net.minecraft.block.entity.BlockEntityType;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemPlacementContext;
-import net.minecraft.server.world.ServerWorld;
 import net.minecraft.state.StateManager;
 import net.minecraft.state.property.BooleanProperty;
 import net.minecraft.state.property.Properties;
+import net.minecraft.state.property.DirectionProperty;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.util.shape.VoxelShapes;
 import net.minecraft.world.BlockView;
 import net.minecraft.world.World;
+import net.minecraft.server.world.ServerWorld;
 
 public class EndRodLampBlock extends BlockWithEntity {
     public static final BooleanProperty LIT = Properties.LIT;
+    public static final DirectionProperty FACING = Properties.FACING;
+
+    // 向きごとの当たり判定
+    private static final VoxelShape SHAPE_UP = Block.createCuboidShape(6, 0, 6, 10, 16, 10);
+    private static final VoxelShape SHAPE_DOWN = SHAPE_UP;
+    private static final VoxelShape SHAPE_NORTH = Block.createCuboidShape(6, 6, 0, 10, 10, 16);
+    private static final VoxelShape SHAPE_SOUTH = SHAPE_NORTH;
+    private static final VoxelShape SHAPE_WEST = Block.createCuboidShape(0, 6, 6, 16, 10, 10);
+    private static final VoxelShape SHAPE_EAST = SHAPE_WEST;
 
     public EndRodLampBlock(AbstractBlock.Settings settings) {
         super(settings);
-        // デフォルトは消灯
-        this.setDefaultState(getDefaultState().with(LIT, false));
+        this.setDefaultState(getDefaultState()
+                .with(LIT, false)
+                .with(FACING, Direction.UP)
+        );
     }
 
-    public static int getLuminance(BlockState currentBlockState) {
-        // LIT プロパティの値を取得し、光度を返す
-        return currentBlockState.get(LIT) ? 15 : 0;
+    public static int getLuminance(BlockState state) {
+        return state.get(LIT) ? 15 : 0;
     }
 
     @Override
@@ -41,9 +53,17 @@ public class EndRodLampBlock extends BlockWithEntity {
         return createCodec(EndRodLampBlock::new);
     }
 
+
     @Override
     protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
-        builder.add(LIT);
+        builder.add(LIT, FACING);
+    }
+
+    @Override
+    public BlockState getPlacementState(ItemPlacementContext ctx) {
+        return this.getDefaultState()
+                .with(LIT, false)
+                .with(FACING, ctx.getPlayerLookDirection().getOpposite());
     }
 
     @Override
@@ -58,46 +78,37 @@ public class EndRodLampBlock extends BlockWithEntity {
     }
 
     @Override
-    public BlockState getPlacementState(ItemPlacementContext ctx) {
-        return this.getDefaultState().with(LIT, false);
+    protected VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext ctx) {
+        return switch (state.get(FACING)) {
+            case UP -> SHAPE_UP;
+            case DOWN -> SHAPE_DOWN;
+            case NORTH -> SHAPE_NORTH;
+            case SOUTH -> SHAPE_SOUTH;
+            case WEST -> SHAPE_WEST;
+            case EAST -> SHAPE_EAST;
+        };
+    }
+
+    @Override
+    protected VoxelShape getCollisionShape(BlockState state, BlockView world, BlockPos pos, ShapeContext ctx) {
+        return getOutlineShape(state, world, pos, ctx);
     }
 
     @Override
     protected ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, BlockHitResult hit) {
-        // クライアント側では SUCCESS を返しておくだけ
-        if (world.isClient) {
-            return ActionResult.SUCCESS;
-        }
-        if (!player.getAbilities().allowModifyWorld) {
-            // Skip if the player isn't allowed to modify the world.
-            return ActionResult.PASS;
-        } else {
-            // サーバー側で PowerNetwork のフラグをトグル
-            ServerWorld sw = (ServerWorld) world;
-            PowerNetwork net = PowerNetwork.get(sw);
-            boolean newState = !net.getStreetlightsEnabled();
+        if (world.isClient) return ActionResult.SUCCESS;
+        if (!player.getAbilities().allowModifyWorld) return ActionResult.PASS;
 
-            // テスト用なので世界とフラグを渡して一斉更新
-            net.setStreetlightsEnabled(newState);
+        ServerWorld sw = (ServerWorld) world;
+        PowerNetwork net = PowerNetwork.get(sw);
+        boolean newState = !net.getStreetlightsEnabled();
+        net.setStreetlightsEnabled(newState);
 
-            return ActionResult.CONSUME;
-        }
+        return ActionResult.CONSUME;
     }
 
     @Override
     public BlockRenderType getRenderType(BlockState state) {
         return BlockRenderType.MODEL;
-    }
-
-    private static final VoxelShape SHAPE = VoxelShapes.fullCube();
-
-    @Override
-    protected VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext ctx) {
-        return SHAPE;
-    }
-
-    @Override
-    protected VoxelShape getCollisionShape(BlockState state, BlockView world, BlockPos pos, ShapeContext ctx) {
-        return SHAPE;
     }
 }
